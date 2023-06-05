4041f32f6a6df8b388cb2e8ee7aefade3687c657
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-05 16:24:51 +0100

    Fix file

--- ferramenta/components/cp_edit_status_user.php
+++ ferramenta/components/cp_edit_status_user.php
@@ -21,24 +21,111 @@ $users = array();
 //PREPARA O STATEMENT
 if (mysqli_stmt_prepare($stmt, $query)) {
 
-    //DÁ BIND DE RESULTADOS (NOME IMAGEM)
-    mysqli_stmt_bind_result($stmt, $username, $date, $active);
-
-    if ($active == 1) {
-        $active = "ativo";
-    } else if ($active == 0) {
-        $active = "inativo";
-    }
-
     //EXECUTA O STATEMENT
     if (mysqli_stmt_execute($stmt)) {
 
-        //VAI BUSCAR TODOS OS DADOS
-        while (mysqli_stmt_fetch($stmt)) {
-        }
+        //DÁ BIND DE RESULTADOS (NOME IMAGEM)
+        mysqli_stmt_bind_result($stmt, $username, $date, $active);
+
+?>
+
+        <body id="page-top">
+
+            <!-- Page Wrapper -->
+            <div id="wrapper">
+
+                <!--CHAMA O NAVBAR -->
+                <?php
+                include_once "cp_sidebar.php";
+                ?>
+
+                <!-- Content Wrapper -->
+                <div id="content-wrapper" class="d-flex flex-column">
+
+                    <!-- Main Content -->
+                    <div id="content">
+
+                        <!--CHAMA O NAVBAR -->
+                        <?php
+                        include_once "cp_navbar.php";
+                        ?>
+
+                        <!-- Begin Page Content -->
+                        <div class="container-fluid border border-info rounded shadow mt-5 mb-5" style="width: 80%;">
+                            <div class="form-header mt-3">
+                                <h2 class="text-center text-dark">Utilizadores sem login à um mês</h2>
+                            </div>
+                            <form method="post" action="./scripts/sc_update_user_status.php">
+                                <div class="form-row mb-5">
+
+                                    <!-- Tabela com os usuários ativos -->
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Nome</th>
+                                                <th>último login</th>
+                                                <th>Estado</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Loop para exibir os resultados
+                                            while (mysqli_stmt_fetch($stmt)) {
+                                                echo "<tr>";
+                                                echo "<td>" . $username . "</td>";
+                                                echo "<td>" . $date . "</td>";
+                                                echo "<td>
+                                                <label for='active'></label>
+                                                <select name='active[]' id='active'>
+                                                    <option value='1'" . ($active == 1 ? " selected" : "") . ">Ativo</option>
+                                                    <option value='0'" . ($active == 0 ? " selected" : "") . ">Inativo</option>
+                                                </select>
+                                                <button type='submit'>Atualizar</button>
+                                                </td>";
+                                                echo "</tr>";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <?php
+                    include_once "components/cp_footer.php";
+                    ?>
+                </div>
+
+                <!-- Scroll to Top Button-->
+                <a class="scroll-to-top rounded" href="#page-top">
+                    <i class="fas fa-angle-up"></i>
+                </a>
+
+                <!--CHAMA O MODAL -->
+                <?php
+                include_once "components/cp_modal_logout.php";
+                ?>
+
+                <!-- Bootstrap core JavaScript-->
+                <script src="vendor/jquery/jquery.min.js"></script>
+                <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+                <!-- Core plugin JavaScript-->
+                <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+                <!-- Custom scripts for all pages-->
+                <script src="js/sb-admin-2.min.js"></script>
+
+                <!-- Page level plugins -->
+                <script src="vendor/chart.js/Chart.min.js"></script>
+
+                <!-- Page level custom scripts -->
+                <script src="js/demo/chart-area-demo.js"></script>
+            </div>
+        </body>
+<?php
     } //SE DER ERRO NA EXECUÇÃO
     else {
-
         //VAI PARA A PÁGINA DE ERROS
         header("Location:../errors.php?error=execute");
     }
@@ -47,102 +134,4 @@ else {
     //VAI PARA A PÁGINA DE ERROS
     header("Location:../errors.php?error=prepare");
 }
-
 ?>
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!--CHAMA O NAVBAR -->
-        <?php
-        include_once "cp_sidebar.php";
-        ?>
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!--CHAMA O NAVBAR -->
-                <?php
-                include_once "cp_navbar.php";
-                ?>
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid border border-info rounded shadow mt-5 mb-5" style="width: 80%;">
-                    <div class="form-header mt-3">
-                        <h2 class="text-center text-dark">Utilizadores sem login à um mês</h2>
-                    </div>
-                    <form method="post" action="./scripts/sc_update_user_status.php">
-                        <div class="form-row mb-5">
-
-                            <!-- Tabela com os usuários ativos -->
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Nome</th>
-                                        <th>último login</th>
-                                        <th>Estado</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    // Loop para exibir os resultados
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        echo "<tr>";
-                                        echo "<td>" . $username . "</td>";
-                                        echo "<td>" . $date . "</td>";
-                                        echo "<td>
-                                        <form method='POST' action=''>
-                                            <label for='active'></label>
-                                            <select name='active' id='active'>
-                                                <option value='1'>Ativo</option>
-                                                <option value='0'>Inativo</option>
-                                            </select>
-                                            <button type='submit'>Atualizar</button>
-                                        </form>
-                                        </td>";
-                                        echo "</tr>";
-                                    }
-                                    ?>
-                                </tbody>
-
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <?php
-            include_once "components/cp_footer.php";
-            ?>
-        </div>
-
-        <!-- Scroll to Top Button-->
-        <a class="scroll-to-top rounded" href="#page-top">
-            <i class="fas fa-angle-up"></i>
-        </a>
-
-        <!--CHAMA O MODAL -->
-        <?php
-        include_once "components/cp_modal_logout.php";
-        ?>
-
-        <!-- Bootstrap core JavaScript-->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        <!-- Core plugin JavaScript-->
-        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-        <!-- Custom scripts for all pages-->
-        <script src="js/sb-admin-2.min.js"></script>
-
-        <!-- Page level plugins -->
-        <script src="vendor/chart.js/Chart.min.js"></script>
-
-        <!-- Page level custom scripts -->
-        <script src="js/demo/chart-area-demo.js"></script>
-    </div>
-</body>
