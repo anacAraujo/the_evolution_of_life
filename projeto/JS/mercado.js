0db42e6283c11b770c9a6fbd5746801a454aad8c
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-11 17:05:33 +0100

    Add events

--- projeto/JS/mercado.js
+++ projeto/JS/mercado.js
@@ -1,15 +1,31 @@
-function mudarOpacidade() {
-    document.getElementById("mercado_barracas_comprar").style.opacity = "100%";
-}
+function showOpcaoVenda() {
+    document.getElementById("mercado_opcoes_venda").style.display = "block";
 
-function vender() {
+    document.getElementById("mercado_opcoes_venda_vender").onclick = function () {
 
-}
-
-function showMercado() {
-    document.getElementById("mercado_ver_mercado").onclick = function () {
-        console.log("1");
-        mudarOpacidade();
     }
 
+    document.getElementById("mercado_opcoes_venda_cancelar").onclick = function () {
+        document.getElementById("mercado_barracas_comprar").style.display = "block";
+        document.getElementById("mercado_ver_mercado").style.display = "block";
+        document.getElementById("mercado_opcoes_venda").style.display = "none";
+    }
+}
+
+
+function mercadoEventos() {
+    document.getElementById("mercado_vender").onclick = function () {
+        document.getElementById("mercado_barracas_comprar").style.display = "none";
+        document.getElementById("mercado_ver_mercado").style.display = "none";
+        showOpcaoVenda();
+    }
+
+    document.getElementById("mercado_ver_mercado").onclick = function () {
+        document.getElementById("mercado_barracas_comprar").style.opacity = "100%";
+    }
+
+}
+
+window.onload = function () {
+    mercadoEventos();
 }
