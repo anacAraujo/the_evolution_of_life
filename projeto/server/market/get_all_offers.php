1088522b9ab5bab9eaab451d1e0e26e41a7f79a0
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-11 23:50:45 +0100

    Fix querie and verification

--- projeto/server/market/get_all_offers.php
+++ projeto/server/market/get_all_offers.php
@@ -6,14 +6,26 @@ include_once "../connections/connection.php";
 
 $conn = new_db_connection();
 
-$sql = "select * from market_offers";
+// TODO complete query select data
+$sql = "SELECT mo.id AS offer_id, my_it.id AS my_item_id, my_it.name AS my_item_name,
+            other_it.id AS other_item_id, other_it.name AS other_item_name
+        FROM market_offers mo
+            INNER JOIN items my_it ON mo.my_item_id = my_it.id
+            INNER JOIN items other_it ON mo.other_item_id = other_it.id
+        WHERE mo.completed = 0";
 
 $result = $conn->query($sql);
 
-$response = array();
+if (!$result) {
+    echo ("Error description: " . $conn->error);
+} else if ($result->num_rows > 0) {
+    $response = array();
 
-while ($row = $result->fetch_assoc()) {
-    $response[] = $row;
+    while ($row = $result->fetch_assoc()) {
+        $response[] = $row;
+    }
+
+    echo json_encode($response);
+} else {
+    echo json_encode(['msg' => 'No Data!', 'status' => false]);
 }
-
-echo json_encode($response);
