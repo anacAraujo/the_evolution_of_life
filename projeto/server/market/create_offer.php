7975766dec654dfa9f7883e17e27020ec3d1fd7a
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-12 15:43:17 +0100

    Add items

--- projeto/server/market/create_offer.php
+++ projeto/server/market/create_offer.php
@@ -4,11 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
-// Since we are inserting data we pass two extra headers.
-// 1st allow us to set the method of insert. i.e. POST in rest api
-// 2nd determines which type of headers can be sent. It's a secuirty header.
-// 'Authorization' is set for authorizing insert data. While 'X-Requested-With' is set for passing data as json
-
 $data = json_decode(file_get_contents("php://input"), true);
 
 $my_item_id = $data['my_item_id'];
@@ -42,5 +37,5 @@ if (mysqli_num_rows($result) > 0) {
 
     $stmt->close();
 } else {
-    echo json_encode(['msg' => 'No Data!', 'status' => false]);
+    echo json_encode(['status' => false, 'msg' => 'No items!']);
 }
