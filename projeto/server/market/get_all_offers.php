ad75941259899b99a022f6aaf6cb5c2932091bf5
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-12 14:54:12 +0100

    Fix querie

--- projeto/server/market/get_all_offers.php
+++ projeto/server/market/get_all_offers.php
@@ -6,12 +6,8 @@ include_once "../connections/connection.php";
 
 $conn = new_db_connection();
 
-// TODO complete query select data
-$sql = "SELECT mo.id AS offer_id, my_it.id AS my_item_id, my_it.name AS my_item_name,
-            other_it.id AS other_item_id, other_it.name AS other_item_name
+$sql = "SELECT *
         FROM market_offers mo
-            INNER JOIN items my_it ON mo.my_item_id = my_it.id
-            INNER JOIN items other_it ON mo.other_item_id = other_it.id
         WHERE mo.completed = 0";
 
 $result = $conn->query($sql);
@@ -26,6 +22,4 @@ if (!$result) {
     }
 
     echo json_encode($response);
-} else {
-    echo json_encode(['msg' => 'No Data!', 'status' => false]);
 }
