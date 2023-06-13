d767b574af8e1ad970c4014c058618d0f0d8983c
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-13 17:28:56 +0100

    Refactor file

--- projeto/server/users/create_user.php
+++ projeto/server/users/create_user.php
@@ -4,36 +4,42 @@ header('Access-Control-Allow-Origin: *');
 
 require_once "../connections/connection.php";
 
-if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["login"]) && isset($_POST["password"])) {
+if (isset($_POST["username"]) && isset($_POST["password"])) {
     $username = $_POST['username'];
-    $email = $_POST['email'];
-    $login = $_POST['login'];
     $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     $link = new_db_connection();
 
     $stmt = mysqli_stmt_init($link);
-    // Antes de inserir deve fazer-se uma consulta à BD para verificar se o username ou email já existem na BD
 
-    $query = "INSERT INTO utilizadores (nome, email, login, password_hash) VALUES (?,?,?,?)";
+    // Verify the user has the items
+    $sql = "SELECT username
+        FROM users
+        WHERE username = ?";
 
-    if (mysqli_stmt_prepare($stmt, $query)) {
-        mysqli_stmt_bind_param($stmt, 'ssss', $username, $email, $login, $password_hash);
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
 
-        // Devemos validar também o resultado do execute!
-        if (mysqli_stmt_execute($stmt)) {
-            // Acção de sucesso
-            header("Location: ../login.php");
-        } else {
-            // Acção de erro
-            header("Location: ../registo.php");
-        }
-    } else {
-        // Acção de erro
-        echo "Error:" . mysqli_error($link);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $stmt->close();
+
+    if ($result->num_rows <= 0) {
+        echo json_encode(['status' => false, 'message' => 'Username already exists.']);
+        return;
     }
-    mysqli_stmt_close($stmt);
-    mysqli_close($link);
-} else {
-    echo "Campos do formulário por preencher";
+
+    // Create the offer
+    $sql = "INSERT INTO utilizadores (nome, email, login, password_hash) VALUES (?,?,?,?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ssss', $username, $email, $login, $password_hash);
+
+    $stmt->execute();
+
+    echo json_encode(['status' => true, 'message' => 'Offer inserted successfully.']);
+
+    $stmt->close();
 }
