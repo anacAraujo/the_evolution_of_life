71f45ab0c4892bbd983101679ec96fe2b7b43a27
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-13 18:27:11 +0100

    Refactor code

--- projeto/server/users/create_user.php
+++ projeto/server/users/create_user.php
@@ -4,42 +4,42 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
-$data = json_decode(file_get_contents("php://input"), true);
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$username = $data['username'];
-$pwd_hash = $data['password'];
+    include_once "../connections/connection.php";
 
-include_once "../connections/connection.php";
+    $conn = new_db_connection();
 
-$conn = new_db_connection();
-
-// Verify the user exists
-$sql = "SELECT username
+    // Verify the user exists
+    $sql = "SELECT username
         FROM users
         WHERE username = ?";
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $username);
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
 
-$stmt->execute();
+    $stmt->execute();
 
-$result = $stmt->get_result();
+    $result = $stmt->get_result();
 
-$stmt->close();
+    $stmt->close();
 
-if ($result->num_rows > 0) {
-    echo json_encode(['status' => false, 'message' => 'Username already exists.']);
-    return;
+    if ($result->num_rows > 0) {
+        echo json_encode(['status' => false, 'message' => 'Username already exists.']);
+        return;
+    }
+
+    // Create the user
+    $sql = "INSERT INTO users (username, pwd_hash) VALUES (?,?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ss', $username, $pwd_hash);
+
+    $stmt->execute();
+
+    echo json_encode(['status' => true, 'message' => 'User inserted successfully.']);
+
+    $stmt->close();
 }
-
-// Create the offer
-$sql = "INSERT INTO users (username, pwd_hash) VALUES (?,?)";
-
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('ss', $username, $pwd_hash);
-
-$stmt->execute();
-
-echo json_encode(['status' => true, 'message' => 'User inserted successfully.']);
-
-$stmt->close();
