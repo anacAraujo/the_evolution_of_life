8240538fa255f0d05ab75030dd8a2ef0efa66937
leobastos13 <leocastrobastos@gmail.com>
2023-06-14 17:19:36 +0100

    fix lab

--- projeto/client/js/lab.js
+++ projeto/client/js/lab.js
@@ -28,7 +28,7 @@ function labEventos() {
     document.body.style.backgroundImage = 'url("img/fundo_lab.png")';
     document.body.style.backgroundPosition = "center";
     document.body.style.backgroundSize = "cover";
-    document.body.style.filter = "3px"; 
+    
     
 }
 
