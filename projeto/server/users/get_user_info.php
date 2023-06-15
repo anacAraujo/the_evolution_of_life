00d9755b2f7f1fb49af0e437a615f8ae4e6f5629
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-06-15 11:37:24 +0100

    Add file

---
+++ projeto/server/users/get_user_info.php
@@ -0,0 +1,46 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+include_once "../connections/connection.php";
+
+session_start();
+
+if (!isset($_SESSION["id"])) {
+    echo json_encode([
+        'status' => false,
+        'message' => 'No user logged in.',
+        'code' => 'NO_LOGIN'
+    ]);
+    return;
+}
+
+$user_id = $_SESSION["id"];
+
+$conn = new_db_connection();
+
+// TODO get all user info (planet / user / avatar / etc)
+$sql = "SELECT *
+        FROM
+        WHERE planets_user_id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$stmt->close();
+
+if (!$result) {
+    echo json_encode(['status' => false, 'message' => $conn->error]);
+    return;
+}
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode($row);
+}
