117516f1318d486245e68c4c52768514a4c69826
T0mas Sousa <tomasrafael@ua.pt>
2023-06-21 18:48:57 +0100

    Avatar

--- projeto/client/avatar.php
+++ projeto/client/avatar.php
@@ -65,9 +65,22 @@ else {
             d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
     </svg>
     </a>
+
+    <?php 
+    if(isset($return)) {
+
+        echo '<!--Fórmulário de Imagens-->
+        <form method="post" action="../server/avatar/update_user_avatar.php?origin=mercado">';
+    }
+    else {
+        echo '<!--Fórmulário de Imagens-->
+        <form method="post" action="../server/avatar/update_user_avatar.php">';
+    }
     
-    <!--Fórmulário de Imagens-->
-    <form method="post" action="../server/avatar/update_user_avatar.php">
+    
+    ?>
+    
+    
         
 <?php
 //FAZ LIGAÇÃO À BASE DE DADOS
