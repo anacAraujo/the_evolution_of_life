8fd356544e9a29bbcd04e0047d1bc2110fe3fad1
T0mas Sousa <tomasrafael@ua.pt>
2023-06-21 16:37:20 +0100

    Go back arrows

--- projeto/client/avatar.php
+++ projeto/client/avatar.php
@@ -1,19 +1,33 @@
 <?php 
 
-//VAI BUSCAR A PÁGINA ANTERIOR
-$previous=$_SERVER['HTTP_REFERER'];
+if(isset($_GET['origin']) && $_GET['origin']!="") {
 
-//MANDA PRO SESSION
-$_SESSION['Avatar_referer'] = $previous;
+    $origin=$_GET['origin'];
 
-//DETERMINA PARA ONDE VAI
-if(isset($_SESSION['Avatar_referer']) && $_SESSION['Avatar_referer']!="") {
+    if($origin=="mercado") {
 
-    $return=$_SESSION['Avatar_referer'];
+        $return="mercado.html";
+
+        //MANDA PRO SESSION
+    $_SESSION['Avatar_referer']=$return;
+    }
+
+    
+
 }
-//ELSE
+else if( isset($_SESSION['Avatar_referer']) && $_SESSION['Avatar_referer'] !="") {
+
+    $origin=$_SESSION['Avatar_referer'];
+
+    if($origin==strpos($origin,"mercado")) {
+
+        $return="mercado.html";
+
+    }
+}
 else {
-    $return=$previous;
+
+    $return="index.html";
 }
 
 ?>
@@ -44,9 +58,9 @@ else {
 
 <!--DIV COM PAINEL DE ESCOLHA-->
 <div class="col-8" id="Avatar_form">
-    <a href=<?=$return?>>
+    <a href=<?=$return?> class="ps-2">
     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-        class="bi bi-arrow-left mt-2" id="Avatar_go_back" viewBox="0 0 16 16">
+        class="bi bi-arrow-left planeta_quantidade_elementos_seta" id="Avatar_go_back" viewBox="0 0 16 16">
         <path fill-rule="evenodd"
             d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
     </svg>
